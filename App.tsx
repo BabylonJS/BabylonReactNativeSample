50bc971aad99e626c40259671f5c3b6aa6c0c8e9
Hedwig Doets <hedwig.doets@mendix.com>
2022-06-15 09:26:02 +0200

    Add AR configuration and permissions dependency

--- App.tsx
+++ App.tsx
@@ -5,22 +5,65 @@
  * @format
  */
 
-import React, { useState, FunctionComponent, useEffect } from "react";
-import { SafeAreaView, StatusBar, View, ViewProps } from "react-native";
-import { EngineView, useEngine } from "@babylonjs/react-native";
-import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
-import { Camera } from "@babylonjs/core/Cameras/camera";
-import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
-import "@babylonjs/loaders/glTF";
+import React, {
+  useState,
+  FunctionComponent,
+  useEffect,
+  useCallback,
+} from 'react';
+import {Button, SafeAreaView, StatusBar, View, ViewProps} from 'react-native';
+import {EngineView, useEngine} from '@babylonjs/react-native';
+import {SceneLoader} from '@babylonjs/core/Loading/sceneLoader';
+import {Camera} from '@babylonjs/core/Cameras/camera';
+import {ArcRotateCamera} from '@babylonjs/core/Cameras/arcRotateCamera';
+import '@babylonjs/loaders/glTF';
+import {Scene, WebXRSessionManager, WebXRTrackingState} from '@babylonjs/core';
 
 const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
   const engine = useEngine();
   const [camera, setCamera] = useState<Camera>();
+  const [xrSession, setXrSession] = useState<WebXRSessionManager>();
+  const [trackingState, setTrackingState] = useState<WebXRTrackingState>();
+  const [scene, setScene] = useState<Scene>();
+
+  const onToggleXr = useCallback(() => {
+    (async () => {
+      if (xrSession) {
+        await xrSession.exitXRAsync();
+      } else {
+        if (scene !== undefined) {
+          const xr = await scene.createDefaultXRExperienceAsync({
+            disableDefaultUI: true,
+            disableTeleportation: true,
+          });
+          const session = await xr.baseExperience.enterXRAsync(
+            'immersive-ar',
+            'unbounded',
+            xr.renderTarget,
+          );
+          setXrSession(session);
+          session.onXRSessionEnded.add(() => {
+            setXrSession(undefined);
+            setTrackingState(undefined);
+          });
+
+          setTrackingState(xr.baseExperience.camera.trackingState);
+          xr.baseExperience.camera.onTrackingStateChanged.add(
+            newTrackingState => {
+              setTrackingState(newTrackingState);
+            },
+          );
+        }
+      }
+    })();
+  }, [scene, xrSession]);
 
   useEffect(() => {
     if (engine) {
-      const url = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoxAnimated/glTF/BoxAnimated.gltf";
-      SceneLoader.LoadAsync(url, undefined, engine).then((scene) => {
+      const url =
+        'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/BoxAnimated/glTF/BoxAnimated.gltf';
+      SceneLoader.LoadAsync(url, undefined, engine).then(scene => {
+        setScene(scene);
         scene.createDefaultCameraOrLight(true, undefined, true);
         (scene.activeCamera as ArcRotateCamera).alpha += Math.PI;
         (scene.activeCamera as ArcRotateCamera).radius = 10;
@@ -32,7 +75,11 @@ const EngineScreen: FunctionComponent<ViewProps> = (props: ViewProps) => {
   return (
     <>
       <View style={props.style}>
-        <View style={{ flex: 1 }}>
+        <Button
+          title={xrSession ? 'Stop XR' : 'Start XR'}
+          onPress={onToggleXr}
+        />
+        <View style={{flex: 1}}>
           <EngineView camera={camera} displayFrameRate={true} />
         </View>
       </View>
@@ -44,8 +91,8 @@ const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-        <EngineScreen style={{ flex: 1 }} />
+      <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+        <EngineScreen style={{flex: 1}} />
       </SafeAreaView>
     </>
   );
